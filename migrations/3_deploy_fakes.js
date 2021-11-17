7118bdcfbef68b5c3dccfe588e1e0e1d216a5aab
kongliangzhong <kongliangzhong@gmail.com>
2021-11-17 16:41:45 +0000

    update

--- migrations/3_deploy_fakes.js
+++ migrations/3_deploy_fakes.js
@@ -1,12 +1,14 @@
 const FakeCollection = artifacts.require("./FakeCollection.sol");
 const RandomCollection = artifacts.require("./RandomCollection.sol");
+const NftContract = artifacts.require("./MoodyApesNFT.sol");
 
 module.exports = async (deployer, network, addresses) => {
   console.log("Deploying to " + network);
 
-  const collectionID = 1;
-  const baseURI = "http://moody-static.s3-website.us-east-2.amazonaws.com";
-  const priceLevels = [-10, 0, 10];
+  const collectionID = 3;
+  // const baseURI = "http://moody-static.s3-website.us-east-2.amazonaws.com";
+  const baseURI = "/ipfs/QmaCks5r5gmN4W1PEUF3pjYxy11qfjNX8r4ufXCNY5zTqQ";
+  const priceLevels = [-1, 50];
   const relativeLevels = [-20, 0, 20];
   // await deployer.deploy(
   //   FakeCollection,
@@ -20,11 +22,14 @@ module.exports = async (deployer, network, addresses) => {
 
   await deployer.deploy(
     RandomCollection,
-    2,
+    collectionID,
     baseURI,
     priceLevels,
     relativeLevels,
     {gas: 5000000}
   );
   console.log("RandomCollection contract: " + RandomCollection.address);
+
+  const nftContract = await NftContract.deployed();
+  await nftContract.addCollection(RandomCollection.address);
 };
