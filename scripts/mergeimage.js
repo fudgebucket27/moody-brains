5fe844aacb97c802f9e0360c397240c9fb3c64ef
kongliangzhong <kongliangzhong@gmail.com>
2021-11-17 15:22:00 +0000

    update

--- scripts/mergeimage.js
+++ scripts/mergeimage.js
@@ -1,14 +1,22 @@
 const fs = require("fs");
 const mergeImages = require('merge-images');
 const { Canvas, Image } = require('canvas');
+const { BigNumber } = require("bignumber.js");
 
 async function doMerge20211116() {
     const bgs = ["poor", "rich"];
     const moods = ["normal", "sad", "happy"];
 
-    for (const bg of bgs) {
-        for (const mood of moods) {
+    const collectionId = "3";
+    const tokenId = "1";
+    const tokenDir = calculateTokenId(collectionId, tokenId);
+
+    for (let i = 0; i < bgs.length; i++) {
+        for (let j = 0; j < moods.length; j++) {
+            const bg = bgs[i];
+            const mood = moods[j];
             console.log("generate image with:", bg, mood)
+
             let b64 = await mergeImages([
                 "scripts/nft-materials/version20211116/bg/" + bg + ".png",
                 "scripts/nft-materials/version20211116/hat/baseball.png",
@@ -22,10 +30,30 @@ async function doMerge20211116() {
 
             console.log("b64:", b64);
             b64 = b64.replace(/^data:image\/png;base64,/, "");
-            const resFile = "scripts/nft-materials/version20211116/result/" + bg + "-" + mood + ".png";
-            fs.writeFileSync(resFile, b64, "base64");
+
+            const resDir =  "scripts/nft-materials/version20211116/" + tokenDir + "/" + i + "_" + j + "/";
+            fs.mkdirSync(resDir, { recursive: true });
+
+            const imageFile = bg + "-" + mood + ".png";
+            fs.writeFileSync(resDir + imageFile, b64, "base64");
+
+            const metadata = {
+                name: "" + i + "_" + j,
+                image: imageFile
+            };
+            fs.writeFileSync(resDir + "metadata.json", JSON.stringify(metadata, undefined, 2));
         }
     }
+}
+
+// collectionId: hex string, without 0x
+// tokenId: hex string, without 0x
+function calculateTokenId(collectionId, tokenId) {
+    const pad1 = 8 - collectionId.length;
+    const pad2 = 56 - tokenId.length;
+    const hexStr = "0".repeat(pad1) + collectionId + "0".repeat(pad2) + tokenId;
+    const bn = new BigNumber(hexStr, 16);
+    return bn.toString(10);
 }
 
 async function main() {
