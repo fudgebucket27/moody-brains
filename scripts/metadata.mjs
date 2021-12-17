51aa4d3c7d07c5d99b27dc219366d01d56168482
kongliang <kongliangzhong@gmail.com>
2021-12-17 13:43:33 +0800

    update metadata format

--- scripts/metadata.mjs
+++ scripts/metadata.mjs
@@ -11,41 +11,33 @@ export function genMetadatasForCollection(collectionInfo, imagePath) {
   const result = {
     baseDir,
     nftContractAddress,
-    tokenIds: []
+    tokenIds: new Set()
   };
   for (const tokenInfo of collectionInfo.tokens) {
     const tokenId = calculateTokenId(collectionId, tokenInfo.id);
-    result.tokenIds.push(tokenId);
+    result.tokenIds.add(tokenId);
 
-    const imageUrl = imagePath + tokenInfo.name + "_" + i + "_" + j + ".png";
+    const imageUrl = imagePath + tokenInfo.name + "_" + tokenInfo.i + "_" + tokenInfo.j + ".png";
     const metadata = {
       "name": `Loophead #${tokenInfo.id}`,
+      "description": `A collection of Moody Brains - Loophead #${tokenInfo.id}`,
       "image": imageUrl,
-      "description": "",
       "external_uri": "https://loopring.io",
-      "attributes": {
-        "collection": "MoodyBrains",
-        "series": "Moody Brains - Loopheads",
-        "standard": "ERC1155",
-        "contract_impl": nftContractAddress,
-        "contract_codebase": "https://github.com/Loopring/moody-brains",
-        "counterfactual": false,
-        "minted_by": "Loopring Technology Limited",
-        "minted_on": "Loopring zkRollup Layer 2",
-        "minted_at": "2021-12-23",
-        "price_source_pool": "${uniswapPool}", //合约用的uniswapPool参数
-        "price_source_pair": "${LRC/ETH}", // uniswapPool对应的交易对
-        "price_levels": collectionInfo.baseLevels, // 合约priceLevels的数组
-        "price_level": tokenInfo.i,
-        "price_relative_levels": collectionInfo.relativeLevels, // relativeLevels
-        "price_relative_level": tokenInfo.j,
-        "image_width": "1251px",
-        "image_height": "1251px",
-        "image_weight": tokenInfo.imageSize, //文件大小
-        "designer": "Chong Zhang <zhangchong@loopring.io>",
-        "gender": `${tokenInfo.gender}`,
-        "cache_expiry_seconds": 300
-      }
+      "cache_expiry_seconds": 3600,
+      "attributes": [
+        {
+          "trait_type": "Ticker", 
+          "value": "LRC"
+        },        
+        {
+          "trait_type": "Gender", 
+          "value": "Female"
+        },
+        {
+          "trait_type": "Creator", 
+          "value": "Chong Zhang <zhangchong@loopring.io>"
+        },
+      ]
     };
 
     const metadataDir = baseDir + tokenId + "/" + tokenInfo.i + "_" + tokenInfo.j + "/";
